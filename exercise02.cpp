16d8a6b8f1179fef51f26c075c9473521c792a30
IT21041402 <87411310+IT21041402@users.noreply.github.com>
2021-07-26 03:23:12 +0000

    Added some chords and edited.

--- exercise02.cpp
+++ exercise02.cpp
@@ -14,13 +14,15 @@ int main() {
     cin >> box1Width;
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
-    
+    cout<<endl;
+
      cout << "Enter Box 2 Height : ";
     cin >> box2Height;
     cout << "Enter Box 2 Width : ";
     cin >> box2Width;
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
+    cout<<endl;
     
     totalVolume = volume(box1Height, box1Width, box1Length)
              + volume(box2Height, box2Width, box2Length);
@@ -31,3 +33,7 @@ int main() {
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length)
+{
+   return height*width*length;
+}
